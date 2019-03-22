1807bce56a6f5c4f488d5616b1b7b63862c8cdb1
t.kandibolotskaya <t.kandibolotskaya@bet-makers.com>
2019-03-22 18:47:28 +0200

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './components/users/users.module';
-import { AuthModule } from './components/auth/auth.module';
-import { FilesModule } from './components/files/files.module';
-import { MessagesModule } from './components/messages/messages.module';
-import { ChatsModule } from './components/chats/chats.module';
-import { NotesModule } from './components/notes/notes.module';
-import { RefreshTokensModule } from './components/refresh-tokens/refresh-tokens.module';
-import { FriendshipRequestsModule } from './components/friendship-requests/friendship-requests.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DB_URL } from './config';
 import { CoreModule } from './components/core/core.module';
 import { EntityResolversModule } from './components/entity-resolvers/entity-resolvers.module';
 import { ConfigService } from './components/core/services/config.service';
-
-
 
 @Module({
   imports: [
@@ -22,15 +12,7 @@ import { ConfigService } from './components/core/services/config.service';
       inject: [ConfigService]
     }),
     EntityResolversModule,
-    UsersModule,
-    AuthModule,
     CoreModule,
-    FilesModule,
-    MessagesModule,
-    ChatsModule,
-    NotesModule,
-    RefreshTokensModule,
-    FriendshipRequestsModule
   ],
 })
 export class AppModule {}
